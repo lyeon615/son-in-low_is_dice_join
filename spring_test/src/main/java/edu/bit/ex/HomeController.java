9ba48d74dda3de65f49acc1018af7384c0b89ff3
lyeon615 <lyeon615@gmail.com>
2020-02-13 15:57:08 +0900

    2020-2-13 join test commit

---
+++ spring_test/src/main/java/edu/bit/ex/HomeController.java
@@ -0,0 +1,22 @@
+package edu.bit.ex;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "join2";
+	}
+	@RequestMapping(value = "/join")
+	public String join(Locale locale, Model model) {	
+		return "join2";
+	}
+}
